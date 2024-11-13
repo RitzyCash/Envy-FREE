7c43d0fe9c536ce2883f7ce020da18a082498d73
slthr <lndei1337@gmail.com>
2024-11-12 20:11:17 -0500

    Update EntryPoint.cpp

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -13,13 +13,10 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
         std::thread(EntryPoint).detach();
         SetConsoleTitleA("");
         system("cls");
-
         HWND hwnd = GetConsoleWindow();
         LONG style = GetWindowLong(hwnd, GWL_EXSTYLE);
         SetWindowLong(hwnd, GWL_EXSTYLE, style | WS_EX_LAYERED);
         SetLayeredWindowAttributes(hwnd, 0, 150, LWA_ALPHA);
-
-        // Set the console window to always be on top
         SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
         break;
     }
